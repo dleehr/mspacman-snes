7a654dc83cf2ea3d6bad1cf9ae4acb5d63d6c924
Dan Leehr <leehro@gmail.com>
2021-01-18 18:52:06 -0500

    Update rgb555.py to take args for file names

--- rgb555.py
+++ rgb555.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+from bitplanes import load
+import sys
+
 # first, colors for ms pac-man
 # 00 000000 black
 # 01 fffb01 yellow
@@ -12,29 +15,6 @@
 # 12 fda985 pink inner border
 # 13 d6d6d6 white dots
 
-colors = """
-000000
-fffb01
-ff2500
-202bde
-000000
-000000
-000000
-000000
-000000
-000000
-000000
-000000
-000000
-000000
-000000
-000000
-000000
-fb0007
-fda985
-d6d6d6
-""".splitlines()
-
 def to_components(hex_color):
     return hex_color[0:2], hex_color[2:4], hex_color[4:6]
 
@@ -46,19 +26,30 @@ def five_msb(byte_str):
     byte = int(byte / 8)
     return byte
 
-with open('SpriteColors.pal', 'wb') as f:
-    for color in colors:
-        if not color:
-            continue
-        # split apart 123456 to [12,34,56]
-        comps = to_components(color)
-        # get the 5 msb of each component and recompose to a 15-bit
-        joined = ''.join([fivebits(five_msb(f)) for f in comps[::-1]])
-        # now parse back to int
+def write(colors, filename):
+    with open(filename, 'wb') as f:
+        for color in colors:
+            if not color:
+                continue
+            # split apart 123456 to [12,34,56]
+            comps = to_components(color)
+            # get the 5 msb of each component and recompose to a 15-bit
+            joined = ''.join([fivebits(five_msb(f)) for f in comps[::-1]])
+            # now parse back to int
+            rgb555 = int(joined, 2)
+            print(hex(rgb555))
+            to_write = rgb555.to_bytes(2, byteorder='little')
+            f.write(to_write)
 
-        rgb555 = int(joined, 2)
-        print(hex(rgb555))
-        to_write = rgb555.to_bytes(2, byteorder='little')
-        f.write(to_write)
-# take the 5 most significant bits
-# of each color component and reverse their order
+def check_args():
+    if not len(sys.argv) == 3:
+        print('Usage: ', sys.argv[0], 'input.pxt output.pal')
+        sys.exit(1)
+
+def main():
+    check_args()
+    colors = load(sys.argv[1])
+    write(colors, sys.argv[2])
+
+if __name__ == '__main__':
+    main()
